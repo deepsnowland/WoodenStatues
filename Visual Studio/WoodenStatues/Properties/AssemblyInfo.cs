7776708731a193930badcafafeaf135dfb833347
Thekillergreece <sakislkg@gmail.com>
2023-04-21 13:34:29 +0300

    Added collisions, adjusted blueprint requirements.

--- Visual Studio/WoodenStatues/Properties/AssemblyInfo.cs
+++ Visual Studio/WoodenStatues/Properties/AssemblyInfo.cs
@@ -1,13 +1,1 @@
-﻿using MelonLoader;
-using System.Reflection;
-
-//This is a C# comment. Comments have no impact on compilation.
-
-
-[assembly: AssemblyCopyright($"Created by Thekillergreece")]
-
-
-[assembly: MelonInfo(typeof(WoodenStatues.Class1), "Wooden Statues", "1.5", "Thekillergreece")]
-
-//This tells MelonLoader that the mod is only for The Long Dark.
-[assembly: MelonGame("Hinterland", "TheLongDark")]
+﻿
